25a843e1178ca2ee8a3b485e0f8e32561a4e5f4e
현정 <146141321+pogooooo@users.noreply.github.com>
2023-12-30 23:48:25 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css'
+import './Detail.js'
+import Detail from './Detail.js';
+import Bulletin from './Bulletin.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Detail></Detail>
+      {/* <Bulletin></Bulletin> */}
+    </div>
+  );
+}
+
+export default App;
